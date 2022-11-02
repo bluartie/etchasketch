f3bd9232dab2a5bc4db800f1836dbf3ec2a3c0fe
bluartie <arteaga.jorge3@gmail.com>
2022-11-02 13:20:39 -0500

    add change color function insie sliderVal eventListener to fix color bug

--- script.js
+++ script.js
@@ -1,30 +1,35 @@
-let container = document.querySelector(".container");
 let sliderVal = document.querySelector("input");
-let sliderDisplay = document.querySelector("output");
-let reset = document.querySelector(".resetbutton");
-sliderDisplay.textContent = sliderVal.value + "x" + sliderVal.value;
 
-populateGrid(sliderVal)
+displayGridSize();
+populateGrid(sliderVal);
+changeColors();
 
 sliderVal.addEventListener("input", () => {
-    rows = document.querySelectorAll(".row");
-    sliderDisplay.textContent = sliderVal.value + "x" + sliderVal.value;
+    displayGridSize();
     clearGrid();
     populateGrid(sliderVal);
+    changeColors();
     });
 
 
    
 
-document.querySelectorAll(".row").forEach(row => row.addEventListener("mouseover", () => {
-    row.style.backgroundColor = "black"  
-  }));
 
-document.querySelectorAll(".row").forEach(row =>reset.addEventListener("click", () => (
-    row.style.backgroundColor = "white"
-)));
+function displayGridSize() {
+    let sliderDisplay = document.querySelector("output");
 
-
+    sliderDisplay.textContent = sliderVal.value + "x" + sliderVal.value;
+}
+function changeColors() {
+    document.querySelectorAll(".row").forEach(row => row.addEventListener("mouseover", () => {
+        row.style.backgroundColor = "black"  
+      }));
+    
+      let reset = document.querySelector(".resetbutton");
+    document.querySelectorAll(".row").forEach(row =>reset.addEventListener("click", () => {
+        row.style.backgroundColor = "white"
+    }));
+};
 function clearGrid() {
 document.querySelectorAll(".row").forEach(row => row.remove());
 document.querySelectorAll(".column").forEach(column => column.remove());
@@ -33,7 +38,7 @@ function populateGrid(num) {
     for(let i=0; i<num.value;i++) {
         let column = document.createElement("div")
         column.setAttribute("class", "column")
-        container.appendChild(column);
+        document.querySelector(".container").appendChild(column);
     
         for(let i=0; i<num.value;i++) {
     let row = document.createElement("div");
